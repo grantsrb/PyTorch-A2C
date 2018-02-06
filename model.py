dd3ebf89e58dd952acc699b82c595a290248e371
Satchel Grant <grantsrb@gmail.com>
2018-02-06 01:23:11 -0800

    reduce model size

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 '''
 Simple, sequential convolutional net.
@@ -17,12 +18,11 @@ class Model(nn.Module):
         self.convs = nn.ModuleList([])
         self.dropouts = nn.ModuleList([]) # Used in dense block
 
-        self.bnorm0 = nn.BatchNorm2d(input_space[-3])
-        self.conv1 = self.conv_block(input_space[-3],16)
+        self.conv1 = self.conv_block(input_space[-3],8)
         self.convs.append(self.conv1)
-        self.conv2 = self.conv_block(16, 24, stride=2, bnorm=True)
+        self.conv2 = self.conv_block(8, 8, stride=2, bnorm=True)
         self.convs.append(self.conv2)
-        self.conv3 = self.conv_block(24, 36, stride=2, bnorm=True)
+        self.conv3 = self.conv_block(8, 12, bnorm=True)
         self.convs.append(self.conv3)
 
         self.features = nn.Sequential(*self.convs)
@@ -54,7 +54,7 @@ class Model(nn.Module):
         dropout = nn.Dropout(dropout_p)
         block.append(dropout)
         self.dropouts.append(dropout)
-        block.append(nn.Linear(chan_in, chan_out, bias=False))
+        block.append(nn.Linear(chan_in, chan_out))
         activation=activation.lower()
         if "relu" in activation:
             block.append(nn.ReLU())
@@ -69,13 +69,12 @@ class Model(nn.Module):
         return nn.Sequential(*block)
 
     def forward(self, x):
-        feats = self.bnorm0(x)
-        feats = self.features(feats)
+        feats = self.features(x)
         feats = feats.view(feats.size(0), -1)
         if self.classifier is None:
             self.flat_shape = feats.shape
-            modules = [self.dense_block(feats.size(1), 200, batch_norm=False)]
-            modules.append(self.dense_block(200, 200, batch_norm=False))
+            modules = [self.dense_block(feats.size(1), 200, batch_norm=True)]
+            modules.append(self.dense_block(200, 200, batch_norm=True))
             self.precursor = nn.Sequential(*modules)
             self.classifier = self.dense_block(200,self.output_space,activation="none",batch_norm=False)
             self.evaluator = self.dense_block(200, 1, activation="none", batch_norm=False)
@@ -116,3 +115,20 @@ class Model(nn.Module):
         """
         for param in self.parameters():
             param.requires_grad = calc_bool
+
+    @staticmethod
+    def preprocess(pic, env_type='snake-v0'):
+        if env_type == "Pong-v0":
+            pic = pic[35:195] # crop
+            pic = pic[::2,::2,0] # downsample by factor of 2
+            pic[pic == 144] = 0 # erase background (background type 1)
+            pic[pic == 109] = 0 # erase background (background type 2)
+            pic[pic != 0] = 1 # everything else (paddles, ball) just set to 1
+        if env_type == "snake-v0":
+            new_pic = np.zeros(pic.shape,dtype=np.float32)
+            new_pic[:,:,0][pic[:,:,0]==1] = 1
+            new_pic[:,:,0][pic[:,:,0]==255] = 1.5
+            new_pic[:,:,1][pic[:,:,1]==255] = 0
+            new_pic[:,:,2][pic[:,:,2]==255] = .3
+            pic = np.sum(new_pic, axis=-1)
+        return pic[None].transpose((2,0,1))
