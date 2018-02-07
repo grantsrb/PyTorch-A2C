8389ddbdadcf980d5dca7c2c88745372d8a17dd4
Satchel Grant <grantsrb@gmail.com>
2018-02-07 00:52:56 -0800

    manipulate x in forward so as to take difference of two observations

--- dense_model.py
+++ dense_model.py
@@ -10,20 +10,24 @@ Description: A simple feedforward model with batchnorm.
 """
 
 class Model(nn.Module):
-    def __init__(self, input_dim, action_dim):
+    def __init__(self, input_space, output_space):
         super(Model, self).__init__()
         self.hidden_dim = 200
+        self.input_space = input_space
+        self.output_space = output_space
 
-        self.entry = nn.Linear(input_dim[-1], self.hidden_dim)
+        self.entry = nn.Linear(input_space[-1], self.hidden_dim)
         self.bnorm1 = nn.BatchNorm1d(self.hidden_dim)
         self.hidden = nn.Linear(self.hidden_dim, self.hidden_dim)
         self.bnorm2 = nn.BatchNorm1d(self.hidden_dim)
 
-        self.action_out = nn.Linear(self.hidden_dim, action_dim)
+        self.action_out = nn.Linear(self.hidden_dim, output_space)
         self.value_out = nn.Linear(self.hidden_dim, 1)
 
     def forward(self, x, bnorm=True):
-        fx = F.relu(self.entry(x))
+        x.data[:,1,:] = -x.data[:,1,:]
+        x = torch.sum(x.data, dim=1)
+        fx = F.relu(self.entry(Variable(x)))
         if bnorm: fx = self.bnorm1(fx)
         fx = F.relu(self.hidden(fx))
         if bnorm: fx = self.bnorm2(fx)
@@ -39,14 +43,14 @@ class Model(nn.Module):
             if torch.sum(param.data != param.data) > 0:
                 print(param)
 
-    def req_grad(self, grad_on):
+    def req_grads(self, grad_on):
         """
         Used to turn off and on all gradient calculation requirements for speed.
 
         grad_on - bool denoting whether gradients should be calculated
         """
         for p in self.parameters():
-            p.rquires_grad = grad_on
+            p.requires_grad = grad_on
 
     @staticmethod
     def preprocess(pic, env_type='snake-v0'):
@@ -63,4 +67,4 @@ class Model(nn.Module):
             new_pic[:,:,1][pic[:,:,1]==255] = 0
             new_pic[:,:,2][pic[:,:,2]==255] = .3
             pic = np.sum(new_pic, axis=-1)
-        return pic.ravel()
+        return pic.ravel()[None]
