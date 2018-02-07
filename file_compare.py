22d5c4c0dfd5ab4789a01626cb937bee8503316c
Satchel Grant <grantsrb@gmail.com>
2018-02-06 23:55:17 -0800

    dummy

--- file_compare.py
+++ file_compare.py
@@ -1,115 +1,161 @@
-import sys
-from collector import Collector
-from updater import Updater
-from model import Model
 import torch
 from torch.autograd import Variable
-import gc
-import resource
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
 
-exp_name = 'test'
-env_type = 'snake-v0'
+class Updater():
+    """
+    This class converts the data collected from the rollouts into useable data to update
+    the model. The main function to use is calc_loss which accepts a rollout to
+    add to the global loss of the model. The model isn't updated, however, until calling
+    calc_gradients followed by update_model. If the size of the epoch is restricted by the memory, you can call calc_gradients to clear the graph.
+    """
 
-# Hyperparameters
-gamma = .99 # Reward discount factor
-_lambda = .98 # GAE moving average factor
-n_rollouts = 3 # Number of times to perform rollouts before updating model
-n_envs = 16 # Number of environments
-n_tsteps = 15 # Maximum number of steps to take in an environment for one episode
-val_const = .1 # Scales the value portion of the loss function
-entropy_const = 0.01 # Scales the entropy portion of the loss function
-max_norm = 0.4 # Scales the gradients using their norm
-lr = 1e-3 # Divide by batchsize as a shortcut to averaging the gradient over multiple batches
-n_state_frames = 3 # number of observations to stack for a single environment state
+    def __init__(self, net, lr, entropy_const=0.01, value_const=0.5, gamma=0.99, _lambda=0.98, max_norm=0.5):
+        self.net = net
+        self.optim = optim.RMSprop(self.net.parameters(), lr=lr)
+        self.global_loss = 0 # Used for efficiency in backprop
+        self.entropy_const = entropy_const
+        self.value_const = value_const
+        self.gamma = gamma
+        self._lambda = _lambda
+        self.max_norm = max_norm
 
-# Environment Choices
-grid_size = [15,15]
-n_foods = 2
-unit_size = 4
-action_space = 4
+        # Tracking variables
+        self.pg_loss = 0
+        self.value_loss = 0
+        self.entropy = 0
+        self.loss_count = 0
+        self.info = {}
 
-test = False
-resume = False
-render = False
-if len(sys.argv) > 1:
-    for arg in sys.argv[1:]:
-        str_arg = str(arg)
-        if "gamma=" in str_arg: gamma = float(str_arg[len("gamma="):])
-        if "_lambda=" in str_arg: _lambda = float(str_arg[len("_lambda="):])
-        if "n_rollouts=" in str_arg: n_rollouts = int(str_arg[len("n_rollouts="):])
-        if "n_envs=" in str_arg: n_envs = int(str_arg[len("n_envs="):])
-        if "n_tsteps=" in str_arg: n_tsteps = int(str_arg[len("n_tsteps="):])
-        if "val_const=" in str_arg: val_const = float(str_arg[len("val_const="):])
-        if "entropy_const=" in str_arg: entropy_const = float(str_arg[len("entropy_const="):])
-        if "max_norm=" in str_arg: max_norm = float(str_arg[len("max_norm="):])
-        if "lr=" in str_arg: lr = float(str_arg[len("lr="):])
-        if "grid_size=" in str_arg: grid_size= [int(str_arg[len('grid_size='):]),int(str_arg[len('grid_size='):])]
-        if "n_foods=" in str_arg: n_foods= int(str_arg[len('n_foods='):])
-        if "unit_size=" in str_arg: unit_size= int(str_arg[len('unit_size='):])
-        if "n_state_frames=" in str_arg: n_state_frames= int(str_arg[len('n_state_frames='):])
-        if "env_type=" in str_arg: env_type = str_arg[len('env_type='):]
+    def calc_gradients(self):
+        """
+        Calculates the gradients of each parameter within the net from the global_loss
+        Variable.
 
-        if "exp_name=" in str_arg: exp_name= str_arg[len('exp_name='):]
-        elif "test" in str_arg: test = True
-        elif "resume" in str_arg: resume = True
-        elif "render" in str_arg: render = True
+        Returns the loss as a float
+        """
 
-print("Experiment Name:", exp_name)
-print("Env Name:", env_type)
-print("gamma:", gamma)
-print("_lambda:", _lambda)
-print("n_rollouts:", n_rollouts)
-print("n_envs:", n_envs)
-print("n_tsteps:", n_tsteps)
-print("val_const:", val_const)
-print("entropy_const:", entropy_const)
-print("max_norm:", max_norm)
-print("lr:", lr)
-print("n_state_frames:", n_state_frames)
-print("grid_size:", grid_size)
-print("n_foods:", n_foods)
-print("unit_size:", unit_size)
-print("lr:", lr)
-print("Test:", test)
-print("Resume:", resume)
-print("Render:", render)
+        try:
+            self.global_loss.backward()
+            self.norm = nn.utils.clip_grad_norm(self.net.parameters(), self.max_norm)
+            if self.loss_count > 0:
+                self.info = {"Global Loss":self.global_loss.data[0]/self.loss_count,
+                            "Policy Loss":self.pg_loss/self.loss_count,
+                            "Value Loss":self.value_loss/self.loss_count,
+                            "Entropy":self.entropy/self.loss_count,
+                            "Norm":self.norm}
+            self.global_loss = 0
+            self.pg_loss = 0
+            self.value_loss = 0
+            self.entropy = 0
+            self.loss_count = 0
+        except RuntimeError:
+            print("Attempted to use self.global_loss.backward() when no graph is created yet!")
 
-if test:
-    net_save_file = "test_net.p"
-    optim_save_file = "test_optim.p"
-    log_file = "test_log.txt"
-else:
-    net_save_file = exp_name+"_net.p"
-    optim_save_file = exp_name+"_optim.p"
-    log_file = exp_name+"_log.txt"
+    def calc_loss(self, states, rewards, dones, actions, advantages):
+        """
+        This function accepts the data collected from a rollout, calculates the loss
+        associated with the rollout, and then adds it to the global_loss.
 
+        states - torch FloatTensor of the environment states from the rollouts
+                shape = (n_states, *state_shape)
+        rewards - torch FloatTensor of rewards from the rollouts
+                shape = (n_states,)
+        dones - torch FloatTensor of done signals from the rollouts
+                dones = (n_states,)
+        actions - torch LongTensor or python integer list denoting the actual action
+                indexes taken in the rollout
+        """
 
-collector = Collector(n_envs=n_envs, grid_size=grid_size, n_foods=n_foods, unit_size=unit_size, n_state_frames=n_state_frames, net=None, n_tsteps=n_tsteps, gamma=gamma, env_type=env_type)
-net = Model(collector.state_shape, action_space)
-collector.net = net
-updater = Updater(collector.net, lr, entropy_const=entropy_const, value_const=val_const, gamma=gamma, _lambda=_lambda, max_norm=max_norm)
-if resume:
-    dummy = Variable(torch.ones(1,*collector.state_shape))
-    updater.net.calculate_grads(False)
-    updater.net.forward(dummy)
-    updater.net.calculate_grads(True)
-    updater.net.load_state_dict(torch.load(exp_name+'_net.p'))
-    updater.optim.load_state_dict(torch.load(exp_name+'_optim.p'))
-updater.optim.zero_grad()
+        states = Variable(torch.FloatTensor(states))
+        values, raw_pis = self.net.forward(states)
+        softlog_pis = F.log_softmax(raw_pis, dim=-1)
+        softlog_column = softlog_pis[list(range(softlog_pis.size(0))), actions]
+        advantages = self.discount(advantages, dones, self.gamma*self._lambda)
+        advantages = self.normalize(advantages)
+        pg_step = softlog_column*Variable(torch.FloatTensor(advantages))
+        pg_loss = -torch.mean(pg_step)
 
-epoch = 0
-while True:
-    epoch += 1
-    print("Begin Epoch", epoch, "– T =", collector.T)
-    for rollout in range(n_rollouts):
-        data = collector.get_data(render)
-        updater.calc_loss(*data)
-    updater.update_model()
-    updater.save_model(net_save_file, optim_save_file)
-    updater.print_statistics()
-    print("Average Reward:", collector.avg_reward)
+        value_targets = self.discount(rewards, dones, self.gamma)
+        value_loss =self.value_const*F.mse_loss(values.squeeze(),Variable(torch.FloatTensor(value_targets)))
 
-    # Check for memory leaks
-    gc.collect()
-    max_mem_used = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-    print("Memory Used: {:.2f} memory\n".format(max_mem_used / 1024))
+        softmaxes = F.softmax(raw_pis, dim=-1)
+        entropy_step = softmaxes*softlog_pis
+        entropy = -self.entropy_const*torch.mean(entropy_step)
+
+        self.global_loss += pg_loss + value_loss - entropy
+        self.pg_loss += pg_loss.data[0]
+        self.value_loss += value_loss.data[0]
+        self.entropy += entropy.data[0]
+        self.loss_count += 1
+
+    def discount(self, array, mask, discount_factor):
+        """
+        Dicounts the argued array following the bellman equation.
+
+        array - array to be discounted
+        mask - binary array denoting the end of an episode
+        discount_factor - float between 0 and 1 used to discount the reward
+
+        Returns the discounted array as an ndarray of type np.float32
+        """
+
+        running_sum = 0
+        discounts = np.zeros(len(array))
+        for i in reversed(range(len(array))):
+            if mask[i] == 1: running_sum = 0
+            running_sum = array[i] + discount_factor*running_sum
+            discounts[i] = running_sum
+        return discounts
+
+    def normalize(self, array, mean=None, std=None):
+        """
+        Normalizes the array's values. Optionally pass a specific mean or standard
+        deviation to use for normalization.
+
+        array - 1 dimensional ndarray or torch FloatTensor
+        mean - optional float denoting the mean to use for normalization.
+                If None, the mean will be calculated from the array
+        std - optional float denoting the standard deviation to use for
+              normalization. If None, the standard deviation will be
+              calculated from the array.
+        """
+
+        if mean is None:
+            if type(array) == type(np.array([])):
+                mean = np.mean(array)
+            else:
+                mean = torch.mean(array)
+        if std is None:
+            if type(array) == type(np.array([])):
+                std = np.std(array)
+            else:
+                std = torch.std(array)
+
+        return (array - mean)/(std+1e-7)
+
+    def print_statistics(self):
+        print(" – ".join([key+": "+str(round(val,5)) if "ntropy" not in key else key+": "+str(val) for key,val in self.info.items()]))
+
+    def save_model(self, net_file_name, optim_file_name):
+        """
+        Saves the state dict of the model to file.
+
+        file_name - string name of the file to save the state_dict to
+        """
+        torch.save(self.net.state_dict(), net_file_name)
+        torch.save(self.optim.state_dict(), optim_file_name)
+
+    def update_model(self):
+        """
+        Performs backprop on any residual graphs and then updates the model using gradient
+        descent.
+        """
+
+        self.calc_gradients()
+
+        self.optim.step()
+        self.optim.zero_grad()
