e4dcf0492ab2b4305fbbbf05b785501b760bf775
Satchel Grant <grantsrb@gmail.com>
2018-02-08 11:33:19 -0800

    change numpy approach in prep

--- model.py
+++ model.py
@@ -125,10 +125,10 @@ class Model(nn.Module):
             pic[pic == 109] = 0 # erase background (background type 2)
             pic[pic != 0] = 1 # everything else (paddles, ball) just set to 1
         if env_type == "snake-v0":
-            new_pic = np.zeros(pic.shape,dtype=np.float32)
-            new_pic[:,:,0][pic[:,:,0]==1] = 1
-            new_pic[:,:,0][pic[:,:,0]==255] = 1.5
-            new_pic[:,:,1][pic[:,:,1]==255] = 0
-            new_pic[:,:,2][pic[:,:,2]==255] = .3
-            pic = np.sum(new_pic, axis=-1)
+            new_pic = np.zeros(pic.shape[:2],dtype=np.float32)
+            new_pic[:,:][pic[:,:,0]==1] = 1
+            new_pic[:,:][pic[:,:,0]==255] = 1.5
+            new_pic[:,:][pic[:,:,1]==255] = 0
+            new_pic[:,:][pic[:,:,2]==255] = .3
+            pic = new_pic
         return pic[None]
