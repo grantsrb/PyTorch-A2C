85729dad5c3e137655621c368604780050e1d9bb
Satchel Grant <grantsrb@gmail.com>
2018-02-11 15:02:57 -0800

    remove deprecated parameters, change action_space to be dependent on gym environment

--- entry.py
+++ entry.py
@@ -1,14 +1,17 @@
 import sys
 from collector import Collector
 from updater import Updater
-import dense_model as model
 import torch
 from torch.autograd import Variable
 import numpy as np
 import gc
 import resource
 
-exp_name = 'test'
+# Change your policy file here!
+import dense_model as model
+print("Using dense_model as policy file.")
+
+exp_name = 'default'
 env_type = 'snake-v0'
 
 # Hyperparameters
@@ -21,7 +24,7 @@ val_const = .5 # Scales the value portion of the loss function
 entropy_const = 0.01 # Scales the entropy portion of the loss function
 max_norm = 0.4 # Scales the gradients using their norm
 lr = 1e-4/n_rollouts # Divide by batchsize as a shortcut to averaging the gradient over multiple batches
-n_state_frames = 2 # number of observations to stack for a single environment state
+n_obs_stack = 2 # number of observations to stack for a single environment state
 gae = True
 reinforce = False
 norm_advs = False # Boolean that will normalize the advantages if true
@@ -31,9 +34,7 @@ view_net_input = False # Boolean to watch the actual input to the neural net
 grid_size = [15,15]
 n_foods = 2
 unit_size = 4
-action_space = 1
 
-test = False
 resume = False
 render = False
 if len(sys.argv) > 1:
@@ -49,15 +50,15 @@ if len(sys.argv) > 1:
         if "max_norm=" in str_arg: max_norm = float(str_arg[len("max_norm="):])
         if "lr=" in str_arg: lr = float(str_arg[len("lr="):])
         if "grid_size=" in str_arg: grid_size= [int(str_arg[len('grid_size='):]),int(str_arg[len('grid_size='):])]
-        if "action_space=" in str_arg: action_space= int(str_arg[len('action_space='):])
         if "n_foods=" in str_arg: n_foods= int(str_arg[len('n_foods='):])
         if "unit_size=" in str_arg: unit_size= int(str_arg[len('unit_size='):])
-        if "n_state_frames=" in str_arg: n_state_frames= int(str_arg[len('n_state_frames='):])
+        if "n_obs_stack=" in str_arg: n_obs_stack= int(str_arg[len('n_obs_stack='):])
         if "env_type=" in str_arg: env_type = str_arg[len('env_type='):]
 
         if "exp_name=" in str_arg: exp_name= str_arg[len('exp_name='):]
-        elif "test" in str_arg: test = True
+        elif "resume=False" in str_arg: resume = False
         elif "resume" in str_arg: resume = True
+        elif "render=False" in str_arg: render = False
         elif "render" in str_arg: render = True
         elif "gae=False" in str_arg: gae = False
         elif "gae=True" in str_arg: gae = True
@@ -65,12 +66,8 @@ if len(sys.argv) > 1:
         elif "reinforce=True" in str_arg: reinforce = True
         elif "norm_advs=False" in str_arg: norm_advs = False
         elif "norm_advs=True" in str_arg: norm_advs = True
+        elif "view_net_input=False" in str_arg: view_net_input = False
         elif "view_net_input" in str_arg: view_net_input = True
-
-if env_type == 'snake-v0':
-    action_space = 4
-elif env_type == 'Pong-v0':
-    action_space = 2
 
 if reinforce and gae:
     print("GAE will take precedence over REINFORCE in model updates.\nREINFORCE is effectively False.")
@@ -86,11 +83,10 @@ print("val_const:", val_const)
 print("entropy_const:", entropy_const)
 print("max_norm:", max_norm)
 print("lr:", lr)
-print("n_state_frames:", n_state_frames)
+print("n_obs_stack:", n_obs_stack)
 print("grid_size:", grid_size)
 print("n_foods:", n_foods)
 print("unit_size:", unit_size)
-print("action_space:", action_space)
 print("norm_advs:", norm_advs)
 print("Test:", test)
 print("Resume:", resume)
@@ -98,21 +94,15 @@ print("Render:", render)
 print("GAE:", gae)
 print("REINFORCE:", reinforce)
 
-if test:
-    net_save_file = "test_net.p"
-    optim_save_file = "test_optim.p"
-    log_file = "test_log.txt"
-else:
-    net_save_file = exp_name+"_net.p"
-    optim_save_file = exp_name+"_optim.p"
-    log_file = exp_name+"_log.txt"
+net_save_file = exp_name+"_net.p"
+optim_save_file = exp_name+"_optim.p"
+log_file = exp_name+"_log.txt"
 
-
-collector = Collector(n_envs=n_envs, grid_size=grid_size, n_foods=n_foods, unit_size=unit_size, n_state_frames=n_state_frames, net=None, n_tsteps=n_tsteps, gamma=gamma, env_type=env_type, preprocessor=model.Model.preprocess)
+collector = Collector(n_envs=n_envs, grid_size=grid_size, n_foods=n_foods, unit_size=unit_size, n_obs_stack=n_obs_stack, net=None, n_tsteps=n_tsteps, gamma=gamma, env_type=env_type, preprocessor=model.Model.preprocess)
 print("Obs Shape:,",collector.obs_shape)
 print("Prep Shape:,",collector.prepped_shape)
 print("State Shape:,",collector.state_shape)
-net = model.Model(collector.state_shape, action_space, env_type=env_type, view_net_input=view_net_input)
+net = model.Model(collector.state_shape, collector.action_space, env_type=env_type, view_net_input=view_net_input)
 dummy = net.forward(Variable(torch.zeros(2,*collector.state_shape)))
 collector.net = net
 updater = Updater(collector.net, lr, entropy_const=entropy_const, value_const=val_const, gamma=gamma, _lambda=_lambda, max_norm=max_norm, norm_advs=norm_advs)
