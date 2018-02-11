a07936e41434de88476fd31f551e1dfd72fcb8e4
Satchel Grant <grantsrb@gmail.com>
2018-02-10 23:02:48 -0800

    update tests

--- updater_unittests.py
+++ updater_unittests.py
@@ -2,7 +2,7 @@ from updater import Updater
 import unittest
 import torch
 import numpy as np
-from model import Model
+from dense_model import Model
 from torch.autograd import Variable
 
 class UpdaterTests(unittest.TestCase):
@@ -10,12 +10,13 @@ class UpdaterTests(unittest.TestCase):
     if torch.cuda.is_available():
         torch.FloatTensor = torch.cuda.FloatTensor
         torch.LongTensor = torch.cuda.LongTensor
+    torch.manual_seed(1)
 
     n_envs=1
     grid_size=[15,15]
     n_foods=1
     unit_size=10
-    n_state_frames=3
+    n_state_frames=2
     n_tsteps=15
     action_space = 4
     gamma = .99
@@ -23,25 +24,32 @@ class UpdaterTests(unittest.TestCase):
     lr = 0.001
     value_const = 0.5
     entropy_const = 0.01
-    state_shape = 9,150,150
-    net = Model(state_shape, action_space)
-    data = torch.FloatTensor(torch.rand(1,*state_shape))
+    state_shape = (2,600)
+    net = Model(state_shape, action_space, env_type='snake-v0')
+    test_data = torch.FloatTensor(torch.ones(2,*state_shape))
+    _,__ = net.forward(Variable(test_data))
+
 
     def test_calc_gradients(self):
         updater = Updater(self.net, self.lr, entropy_const=self.entropy_const, value_const=self.value_const, gamma=self.gamma, _lambda=self._lambda)
-        net = Model(self.state_shape, self.action_space)
-        net.load_state_dict(updater.net.state_dict())
 
-        v,p = updater.net.forward(Variable(self.data))
-        updater.global_loss = v + torch.sum(p)
+        net = Model(self.state_shape, self.action_space, env_type='snake-v0')
+        _,__ = net.forward(Variable(self.test_data))
+        net.load_state_dict(updater.net.state_dict())
+        
+        for p1,p2 in zip(updater.net.named_parameters(), net.named_parameters()):
+            self.assertTrue(np.array_equal(p1[1].data.numpy(), p2[1].data.numpy()))
+
+        v,p = updater.net.forward(Variable(self.test_data.clone()))
+        updater.global_loss = torch.sum(v) + torch.sum(p)
         updater.calc_gradients()
 
-        v,p = net.forward(Variable(self.data))
-        loss = v + torch.sum(p)
+        v,p = net.forward(Variable(self.test_data.clone()))
+        loss = torch.sum(v) + torch.sum(p)
         loss.backward()
 
-        for p1,p2 in zip(updater.net.parameters(), net.parameters()):
-            self.assertTrue(np.array_equal(p1.grad.data.numpy(), p2.grad.data.numpy()))
+        for p1,p2 in zip(updater.net.named_parameters(), net.named_parameters()):
+            self.assertTrue(np.array_equal(p1[1].grad.data.numpy(), p2[1].grad.data.numpy()))
 
     def test_discount(self):
         updater = Updater(self.net, self.lr, entropy_const=self.entropy_const, value_const=self.value_const, gamma=self.gamma, _lambda=self._lambda)
