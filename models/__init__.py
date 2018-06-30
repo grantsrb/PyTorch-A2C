a0e18d6170a513ba632a3f0c897c61336456e7b0
Saetch Grant <grantsrb@gmail.com>
2018-06-29 20:21:46 -0700

    nuke contents

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .conv_model import ConvModel
-from .fc_model import FCModel
-from .a3c_model import A3CModel
-from .gru_model import GRUModel
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 main.py exp_name=testing resume=False env_type=Breakout-v0 n_envs=2 n_rollouts=2 n_tsteps=32
